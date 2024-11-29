65a2f40bc33db24759e9d0e8e5395f7f6f5722bc
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2024-11-29 15:43:13 +0100

    Final Edit

--- src/index.js
+++ src/index.js
@@ -116,14 +116,19 @@ function Menu (){
 function Pizza({pizzaObj}) {
   console.log(pizzaObj)
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}> 
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
 
       <div>
         <h3>{pizzaObj.name}</h3>
 
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+
+        {/* {pizzaObj.soldOut ? (<span>SOLD OUT</span>) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+        <span>{pizzaObj.soldOut ?  " Sold Out" : pizzaObj.price }</span>
       </div>
     </li>
   );
